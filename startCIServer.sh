a755c2c9aacdedf47fe533c1a0d680e13c9f1e35
Guus der Kinderen <guus.der.kinderen@gmail.com>
2024-03-05 15:27:51 +0100

    Remove unused config options

--- startCIServer.sh
+++ startCIServer.sh
@@ -8,23 +8,15 @@ HOST='example.org'
 
 usage()
 {
-	echo "Usage: $0 [-d] [-l] [-i IPADDRESS] [-h HOST]"
-	echo "    -d: Enable debug mode. Prints commands, and preserves temp directories if used (default: off)"
-	echo "    -l: Launch a local Openfire. (default: off)"
+	echo "Usage: $0 [-i IPADDRESS] [-h HOST] [-b BASEDIR]"
 	echo "    -i: Set a hosts file for the given IP and host (or for example.com if running locally). Reverted at exit."
 	echo "    -h: The hostname for the Openfire under test (default: example.org)"
         echo "    -b: The base directory of the distribution that is to be started"
 	exit 2
 }
 
-while getopts dlh:i:b: OPTION "$@"; do
+while getopts h:i:b: OPTION "$@"; do
 	case $OPTION in
-		d)
-			set -x
-			;;
-		l)
-			LOCAL_RUN=true
-			;;
 		h)
 			HOST="${OPTARG}"
 			;;
@@ -39,28 +31,6 @@ while getopts dlh:i:b: OPTION "$@"; do
         *  ) usage;;
 	esac
 done
-
-if [[ $LOCAL_RUN == true ]] && [[ $HOST != "example.org" ]]; then
-	echo "Host is fixed if launching a local instance. If you have an already-running instance to test against, omit the -l flag (and provide -i 127.0.0.1 if necessary)."
-	exit 1
-fi
-
-function setBaseDirectory {
-	# Pretty fancy method to get reliable the absolute path of a shell
-	# script, *even if it is sourced*. Credits go to GreenFox on
-	# stackoverflow: http://stackoverflow.com/a/12197518/194894
-	pushd . > /dev/null
-	SCRIPTDIR="${BASH_SOURCE[0]}";
-	while [ -h "${SCRIPTDIR}" ]; do
-		cd "$(dirname "${SCRIPTDIR}")"
-		SCRIPTDIR="$(readlink "$(basename "${SCRIPTDIR}")")";
-	done
-	cd "$(dirname "${SCRIPTDIR}")" > /dev/null
-	SCRIPTDIR="$(pwd)";
-	popd  > /dev/null
-	BASEDIR="${SCRIPTDIR}"
-	cd "${BASEDIR}"
-}
 
 function setHostsFile {
 	if [[ -n "${IPADDRESS-}" ]]; then
@@ -90,10 +60,8 @@ function launchOpenfire {
 	timeout 120 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 0.3; done' localhost 7070
 }
 
-#setBaseDirectory
 if [[ -n "${IPADDRESS-}" ]]; then
 	setHostsFile
 fi
-if [[ $LOCAL_RUN == true ]]; then
-	launchOpenfire
-fi
+
+launchOpenfire
